c8488d376a44bc554315aa5fc680984cfcf1318c
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-28 10:53:19 -0300

    imported component to index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { JournalApp } from './JournalApp'
 
 ReactDOM.render(
-    <App />,
+    <JournalApp />,
   document.getElementById('root')
 );
