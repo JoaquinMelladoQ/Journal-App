6de0e76ac2b876d4f02717623faac8b1e9b45ba4
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-28 20:29:39 -0300

    added a url img

--- src/Components/Journal/JournalEntry.js
+++ src/Components/Journal/JournalEntry.js
@@ -3,7 +3,13 @@
  export const JournalEntry = () => {
      return (
          <div className="journal__entry">
-             <div className="journal__entry-picture">
+             <div 
+                className="journal__entry-picture"
+                style={{
+                    backgroundSize: 'cover',
+                    backgroundImage: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOCCCeB3plBM6Ad9zWJqLl6Zj5G7F-rFk8bw&usqp=CAU)'
+                }}
+             >
 
              </div>
          </div>
