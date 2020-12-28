4841f0650d5840d96d7156154ced251f3e11af76
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-28 15:03:09 -0300

    sign in was added

--- src/Components/Auth/LoginScreen.js
+++ src/Components/Auth/LoginScreen.js
@@ -23,7 +23,21 @@ export const LoginScreen = () => {
                     Login
                 </button>
                 <hr />
-                Google
+
+                <div>
+                    <p>Login with social networks</p>
+                    <div 
+                        className="google-btn"
+                    >
+                        <div className="google-icon-wrapper">
+                            <img className="google-icon" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" alt="google button" />
+                        </div>
+                        <p className="btn-text">
+                            <b>Sign in with google</b>
+                        </p>
+                    </div>  
+                </div>
+                
             </form>
         </>
     )
