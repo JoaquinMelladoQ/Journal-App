d3559f9a203c53c422761e4a769043e1340b9716
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-28 20:03:59 -0300

    font awesome styles

--- src/Components/Journal/Sidebar.js
+++ src/Components/Journal/Sidebar.js
@@ -2,8 +2,19 @@ import React from 'react'
 
 export const Sidebar = () => {
     return (
-        <div>
-            sidebar
-        </div>
+        <aside className="journal__sidebar">
+            <div className="journal__sidebar-navbar">
+                <h3 className="mt-5">
+                    <i className="far fa-moon"></i>
+                    <span> jamq</span>
+                </h3>
+                <button className="btn">
+                    Logout
+                </button>
+            </div>
+                <div className="journal__new-entry">
+                    <i className="far fa-calendar-plus fa-5x"></i>
+                </div>
+        </aside>
     )
 }
