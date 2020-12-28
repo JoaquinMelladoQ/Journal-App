505e622bb6fb66cbb1a5302d624eb8b762f9b58c
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-28 20:09:34 -0300

    styles to sidebar

--- src/Components/Journal/Sidebar.js
+++ src/Components/Journal/Sidebar.js
@@ -14,6 +14,9 @@ export const Sidebar = () => {
             </div>
                 <div className="journal__new-entry">
                     <i className="far fa-calendar-plus fa-5x"></i>
+                    <p className="mt-5">
+                        New Entry
+                    </p>
                 </div>
         </aside>
     )
