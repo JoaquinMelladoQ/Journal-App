7a95b912e503218b75e9e7c3e3835d60a5b4737f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-28 12:04:03 -0300

    fixing issues and added settings

--- src/Routers/AuthRouter.js
+++ src/Routers/AuthRouter.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { LoginScreen } from '../Components/Auth/LoginScreen'
 
 export const AuthRouter = () => {
     return (
         <div>
-            
+            <Switch>
+                <Route 
+                    exact
+                    path="/auth/login"
+                    component={ LoginScreen }
+                />
+            </Switch>
         </div>
     )
 }
