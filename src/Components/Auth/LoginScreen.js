a4395420ae2fc21000ab7638ca1494ec12b5cb2f
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-28 15:06:13 -0300

    link added

--- src/Components/Auth/LoginScreen.js
+++ src/Components/Auth/LoginScreen.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginScreen = () => {
     return (
@@ -37,7 +38,9 @@ export const LoginScreen = () => {
                         </p>
                     </div>  
                 </div>
-                
+                <Link to="/auth/register">
+                    Create new account
+                </Link>
             </form>
         </>
     )
