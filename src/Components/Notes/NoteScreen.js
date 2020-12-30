f111d8d33803d0a67ebf87b49743d0c0a55298d0
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-30 13:25:45 -0300

    added an input textarea and img

--- src/Components/Notes/NoteScreen.js
+++ src/Components/Notes/NoteScreen.js
@@ -4,7 +4,29 @@ import { NotesAppBar } from './NotesAppBar'
 export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
+
             <NotesAppBar />
+
+            <div className="notes__content">
+                <input
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input" 
+                />
+
+                <textarea 
+                    placeholder="What happened today?"
+                    className="notes__textarea"
+                >
+                </textarea>
+                <div className="notes__image">
+                    <img 
+                        src="https://www.chileclimbers.cl/wp-content/uploads/2013/02/cochamo.jpg"
+                        alt="Cochamo"
+                    />
+                </div>
+
+            </div>
         </div>
     )
 }
