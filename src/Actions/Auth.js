4d25d9762898f511cdc73b2152bbf9a495b4eb66
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-01-11 10:51:13 -0300

    Added an auth with email and pass from Firebase

--- src/Actions/Auth.js
+++ src/Actions/Auth.js
@@ -3,11 +3,14 @@ import { types } from '../Types/Types'
 
 export const startLoginEmailPassword = (email, password) => {
     return (dispatch) => {
-        setTimeout(() => {
-            
-            dispatch(login( '123', 'aoeu' ))
-
-        }, 2000);
+        firebase.auth().signInWithEmailAndPassword( email, password )
+            .then( ({ user }) => {
+                dispatch(
+                    login( user.uid, user.displayName )
+                )
+            }).catch( e => {
+                console.log(e)
+            } )
     }
 }
 
