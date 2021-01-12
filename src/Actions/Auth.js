b685578a20ea4cde7a1dc912722aeb1b657e6294
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-01-12 13:38:37 -0300

    Integration SweetAlert 2

--- src/Actions/Auth.js
+++ src/Actions/Auth.js
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2'
 import { firebase, googleAuthProvider } from '../Firebase/firebaseConfig'
 import { types } from '../Types/Types'
 import { finishLoading, startLoading } from './Ui'
