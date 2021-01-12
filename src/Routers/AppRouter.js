bcd70e5c7d1ef0af82e9912b75cfc76d8e2fad0c
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2021-01-12 12:33:48 -0300

    Implemented setInLoggedIn, state change from false to true

--- src/Routers/AppRouter.js
+++ src/Routers/AppRouter.js
@@ -15,18 +15,22 @@ export const AppRouter = () => {
 
     const dispatch = useDispatch()
 
-    const [checking, setChecking] = useState(true)
+    const [ checking, setChecking ] = useState(true)
+    const [ isLoggedIn, setIsLoggedIn ] = useState(false)
 
-    useEffect(() => {
-      firebase.auth().onAuthStateChanged((user) =>{
+    useEffect(() => { 
+      firebase.auth().onAuthStateChanged(( user ) =>{
           if ( user?.uid ) {
               dispatch( login(user.uid, user.displayName) )
+              setIsLoggedIn( true )
+          } else {
+            setIsLoggedIn( false )
           }
-          
-          setChecking(false)
+           
+          setChecking( false )
           
       }) 
-    }, [ dispatch, setChecking ])
+    }, [ dispatch, setChecking, setIsLoggedIn ])
 
     if (checking) {
         return (
